30083d38409cc11d7079d8e3f90a6ca7068698e0
MarlenByrd <ananasiknu93@gmail.com>
2025-11-23 22:34:04 +0300

    zadanie 0

---
+++ SQL_Queries/Junior/Day 03 - Piscine SQL/day03_ex00.sql
@@ -0,0 +1,1 @@
+SELECT m.pizza_name, m.price, p.name AS pizzeria_name, pv.visit_date FROM menu m JOIN pizzeria p ON m.pizzeria_id = p.id JOIN person_visits pv ON p.id = pv.pizzeria_id JOIN person pe ON pv.person_id = pe.id WHERE pe.name = 'Kate' AND m.price BETWEEN 800 AND 1000 ORDER BY m.pizza_name, m.price, p.name;
